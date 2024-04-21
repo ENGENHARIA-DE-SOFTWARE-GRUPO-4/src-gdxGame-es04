cf9aae09fba6164a05bc83eb5aa44b30e6c0fa82
sabrina-batista <sabrina.batista@ccc.ufcg.edu.br>
2024-04-21 00:36:40 -0300

    Add tests for hasItem method in InventorySlot

--- core/src/test/java/com/gdx/game/inventory/slot/InventorySlotTest.java
+++ core/src/test/java/com/gdx/game/inventory/slot/InventorySlotTest.java
@@ -2,12 +2,16 @@ package com.gdx.game.inventory.slot;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.gdx.game.GdxRunner;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
 @ExtendWith(GdxRunner.class)
@@ -25,5 +29,25 @@ public class InventorySlotTest {
 
         assertThat(inventorySlot).isNotNull();
         assertThat(inventorySlot.hasItem()).isFalse();
+    }
+
+    @Test
+    public void testHasItem_SlotWithItems() {
+        InventorySlot inventorySlot = new InventorySlot();
+        Actor mockAtor = new Actor();
+        inventorySlot.add(mockAtor);
+        
+        boolean hasItem = inventorySlot.hasItem();
+
+        assertTrue(hasItem);
+    }
+
+    @Test
+    public void testHasItem_EmptySlot() {
+        InventorySlot inventorySlot = new InventorySlot();
+        
+        boolean hasItem = inventorySlot.hasItem();
+
+        assertFalse(hasItem);
     }
 }
